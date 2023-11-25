27fbfb86ba498f98db9dc642b76751f76b38f43a
MoskalykVasyl <moskalikvasiok@gmail.com>
2023-11-25 22:19:46 +0200

    Created endpoints for tasks

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import './config/db.js';
 
 // Routes
 import authRouter from './routers/authRoutes.js';
+import taskRouter from './routers/taskRoutes.js';
+
 
 const app = express();
 const port = 3000;
@@ -13,6 +15,8 @@ const port = 3000;
 app.use(bodyParser.json());
 
 app.use('/api', authRouter)
+app.use('/api', taskRouter)
+
 
 app.listen(port, () => {
     console.log(`Server starting at the http://localhost:${port}`);
